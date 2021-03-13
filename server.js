af7eef881237a972fe4a326cbcd46ce7254fad6b
Nina Jade <jaderiver64@gmail.com>
2021-03-13 10:48:53 -0600

    server.js updates

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//const path = require('path');
 //const express = require('express');
 //const session = require('express-session');
 //const exphbs = require('express-handlebars');
@@ -30,16 +29,16 @@
 
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, 'public')));
+
 
 //app.use(require('./controllers/api'));
 
 //sequelize.sync({ force: true }).then(() => {
 //    app.listen(PORT, () => console.log('Now listening'));
 //});
-
+const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
+const controllers = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -47,9 +46,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
-app.use(routes);
+app.use(controllers);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
