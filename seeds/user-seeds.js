8146d425525b1eda26ebe95667c007aec615368f
Nina Jade <jaderiver64@gmail.com>
2021-03-13 09:32:12 -0600

    seeds built

---
+++ seeds/user-seeds.js
@@ -0,0 +1,58 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userdata = [{
+        username: 'ninajoy',
+        email: 'ninajoy@gmail.com',
+        password: 'test'
+    },
+    {
+        username: 'allivee',
+        email: 'allivee@yahoomail.com',
+        password: 'test'
+    },
+    {
+        username: 'lizzybush',
+        email: 'lizzybush@gmail.com',
+        password: 'test'
+    },
+    {
+        username: 'amara',
+        email: 'amara@smegs.net',
+        password: 'test'
+    },
+    {
+        username: 'chuckisgodd',
+        email: 'chuckisgodd@godly.com',
+        password: 'test'
+    },
+    {
+        username: 'notmoose',
+        email: 'deanwinchester@gmail.com',
+        password: 'test'
+    },
+    {
+        username: 'sammywins',
+        email: 'sam.winchester@hunter.com',
+        password: 'test'
+    },
+    {
+        username: 'bobby',
+        email: 'idjuts@jadoomail.com',
+        password: 'test'
+    },
+    {
+        username: 'pamelaposts',
+        email: 'pam.barnes@google.au',
+        password: 'test'
+    },
+    {
+        username: 'buddhabelly',
+        email: 'jadoos.secret@mailbot.com',
+        password: 'test'
+    }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, { individualHooks: true });
+
+module.exports = seedUsers;
