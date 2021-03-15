2d70b26c2c2a8b69da3ca930de8157fd13aede2f
Nina Jade <jaderiver64@gmail.com>
2021-03-15 01:05:51 -0500

    adding login sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
         })
         .then(userData => {
             if (!userData) {
-                res.status(404).json({ message: 'No user found with this id' });
+                res.status(404).json({ message: 'No user found with that id' });
                 return;
             }
             res.json(userData);
@@ -66,18 +66,25 @@ router.post('/login', (req, res) => {
         }
     }).then(userData => {
         if (!userData) {
-            res.status(400).json({ message: 'No user with that email address!' });
+            res.status(400).json({ message: 'No user found with that email address' });
             return;
         }
 
         const validPassword = userData.passwordCheck(req.body.password);
 
         if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password!' });
+            res.status(400).json({ message: 'Password incorrect' });
             return;
         }
 
-        res.json({ user: userData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: userData, message: 'Login successful' });
+        });
     });
 });
 
@@ -91,7 +98,7 @@ router.put('/:id', (req, res) => {
         })
         .then(userData => {
             if (!userData[0]) {
-                res.status(404).json({ message: 'No user found with this id' });
+                res.status(404).json({ message: 'No user found with that id' });
                 return;
             }
             res.json(userData);
@@ -110,7 +117,7 @@ router.delete('/:id', (req, res) => {
         })
         .then(userData => {
             if (!userData) {
-                res.status(404).json({ message: 'No user found with this id' });
+                res.status(404).json({ message: 'No user found with that id' });
                 return;
             }
             res.json(userData);
