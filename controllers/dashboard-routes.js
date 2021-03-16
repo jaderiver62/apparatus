a26d0719a6112346cb8217608cc701a3b1619dbb
Nina Jade <jaderiver64@gmail.com>
2021-03-16 05:44:09 -0500

    dashboard working!

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -38,4 +38,43 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(postData => {
+            if (postData) {
+                const post = postData.get({ plain: true });
+
+                res.render('post-edit', {
+                    post,
+                    loggedIn: true
+                });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
