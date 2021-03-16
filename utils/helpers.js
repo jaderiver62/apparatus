b323579b19e56611e4b7e5fac4c889595b329247
Nina Jade <jaderiver64@gmail.com>
2021-03-15 23:31:45 -0500

    partials and helpers tested and completed

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,7 +2,7 @@ module.exports = {
     date_format: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
       date
-    ).getFullYear()}`;
+    ).getFullYear()} `;
 
     },
     word_format: (word, amount) => {
